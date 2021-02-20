2ae74249b262cc8aabcd499956b654b54bf522be
Tal Melamed <4ppsec@keizer.wind3.hub>
2021-02-20 12:16:45 +0100

    Serverless framework - fixed

--- backend/src/functions/processing/feedback_uploads.py
+++ backend/src/functions/processing/feedback_uploads.py
@@ -8,18 +8,21 @@ from urllib import parse
 
 
 def lambda_handler(event, context):
-    print("This is the event" + json.dumps(event))
+    print(json.dumps(event))
     if "file" in event:
         s3 = boto3.client('s3')
         uuidv4 = str(uuid.uuid4())
         try:
-            response = s3.generate_presigned_post(os.environ["FEEDBACK_BUCKET"], uuidv4 + "_" + event["file"],
-                                                  ExpiresIn=120)
+            response = s3.generate_presigned_post(os.environ["FEEDBACK_BUCKET"], 
+                                                uuidv4 + "_" + event["file"],
+                                                ExpiresIn=120
+                                                )
+            print(response)
         except ClientError as e:
             print(str(e))
             return json.dumps({"status": "err", "msg": "could not get signed url"})
 
-        return json.dumps(response)
+        return response
 
     elif "Records" in event:
         filename = parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"])
